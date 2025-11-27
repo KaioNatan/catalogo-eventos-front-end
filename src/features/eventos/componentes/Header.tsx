6cd1bee8fb5b2b3eb43eef93f06953b31e08277b
Kaio <kaionatan83@gmail.com>
2025-11-27 16:59:31 -0400

    criar componentes Header e Sidebar

---
+++ src/features/eventos/componentes/Header.tsx
@@ -0,0 +1,12 @@
+export interface HeaderProps {
+    open: boolean;
+    handleSideMenuOpen: () => void;
+    handleShowFilters: () => void;
+}
+
+export default function Header({ handleShowFilters, handleSideMenuOpen, open }: HeaderProps) {
+
+    return (
+        <header
+    )
+}
